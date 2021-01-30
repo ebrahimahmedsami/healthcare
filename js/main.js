6d83688ee8d93e39a76ea181093fc4e57e0eb4f6
ebrahimahmedsami <himasami0000@gmail.com>
2021-01-30 16:11:02 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+    
+    $('#home').click(function() {
+    $('input').scrollTop(100);
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+      $("#mainNav .navbar-nav .nav-item .nav-link").css("color","#fff");
+      $("#mainNav .navbar-brand").css("color","#fff");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+      $("#mainNav .navbar-nav .nav-item .nav-link").css("color","#32464a");
+      $("#mainNav .navbar-brand").css("color","#32464a");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
